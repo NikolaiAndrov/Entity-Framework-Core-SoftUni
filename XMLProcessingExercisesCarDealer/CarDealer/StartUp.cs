2c73330c4a59bd70ea149e5e62fe7407b98028f8
nikolaiandrov <nikolaiandrov@abv.bg>
2023-10-23 15:38:35 +0300

    Adding more configurations and solving first exercise problem

--- XMLProcessingExercisesCarDealer/CarDealer/StartUp.cs
+++ XMLProcessingExercisesCarDealer/CarDealer/StartUp.cs
@@ -1,10 +1,59 @@
 ﻿namespace CarDealer
 {
+    using AutoMapper;
+    using CarDealer.Data;
+    using CarDealer.DTOs.Import;
+    using CarDealer.Models;
+    using CarDealer.Utilities;
+
     public class StartUp
     {
         public static void Main()
         {
+            CarDealerContext context = new CarDealerContext();
+            string inputXml = File.ReadAllText("../../../Datasets/suppliers.xml");
 
+            string result = ImportSuppliers(context, inputXml);
+            Console.WriteLine(result);
+        }
+
+        //P09 Import Suppliers
+        public static string ImportSuppliers(CarDealerContext context, string inputXml)
+        {
+            IMapper mapper = CreateMapper();
+
+            XmlHelper xmlHelper = new XmlHelper();
+            ImportSupplierDto[] importSupplierDtos = xmlHelper.Deserialize<ImportSupplierDto[]>(inputXml, "Suppliers");
+
+            ICollection<Supplier> suppliersToAdd = new HashSet<Supplier>();
+
+            foreach (var dto in importSupplierDtos)
+            {
+                if (string.IsNullOrEmpty(dto.Name))
+                {
+                    continue;
+                }
+
+                Supplier supplier = mapper.Map<Supplier>(dto);
+                suppliersToAdd.Add(supplier);
+            }
+
+            context.Suppliers.AddRange(suppliersToAdd);
+            context.SaveChanges();
+
+            return $"Successfully imported {suppliersToAdd.Count}";
+        }
+
+        public static IMapper CreateMapper()
+        {
+            MapperConfiguration mapperConfiguration = new MapperConfiguration(config =>
+            {
+                config.AddProfile<CarDealerProfile>();
+            });
+
+            IMapper mapper = mapperConfiguration.CreateMapper();
+
+            return mapper;
         }
     }
 }
---
+++ XMLProcessingExercisesCarDealer/CarDealer/Utilities/XmlHelper.cs
@@ -0,0 +1,29 @@
+﻿namespace CarDealer.Utilities
+{
+    using System.Xml.Serialization;
+
+    public class XmlHelper
+    {
+        public T Deserialize<T>(string inputXml, string rootName)
+        {
+            XmlRootAttribute xmlRoot = new XmlRootAttribute(rootName);
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(T), xmlRoot);
+
+            using StringReader reader = new StringReader(inputXml);
+            T importDtos = (T)xmlSerializer.Deserialize(reader);
+
+            return importDtos;
+        }
+
+        public T[] DeserializeCollection<T>(string inputXml, string rootName)
+        {
+            XmlRootAttribute xmlRoot = new XmlRootAttribute(rootName);
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(T[]), xmlRoot);
+
+            using StringReader reader = new StringReader(inputXml);
+            T[] importDtos = (T[])xmlSerializer.Deserialize(reader);
+
+            return importDtos;
+        }
+    }
+}
