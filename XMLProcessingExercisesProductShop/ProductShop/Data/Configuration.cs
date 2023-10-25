e00e7110dd2ccecd53c63397b3cba2cbf281f88d
nikolaiandrov <nikolaiandrov@abv.bg>
2023-10-25 16:19:14 +0300

    XML Processing Exercises ProductShop DB

---
+++ XMLProcessingExercisesProductShop/ProductShop/Data/Configuration.cs
@@ -0,0 +1,7 @@
+﻿namespace ProductShop.Data
+{
+    public static class Configuration
+    {
+        public const string ConnectionString = @"Server=localhost;Database=ProductShop;Trusted_Connection=True";
+    }
+}
