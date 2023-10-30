e1f4c1f44d3eee8292c5763792bc1769ddee7fe3
nikolaiandrov <nikolaiandrov@abv.bg>
2023-10-30 22:47:29 +0200

    deleting automapper configuration of the invoice for preventing future issues

--- InvoicesDatabasesAdvancedExam11April2023/Invoices/InvoicesProfile.cs
+++ InvoicesDatabasesAdvancedExam11April2023/Invoices/InvoicesProfile.cs
@@ -16,11 +16,6 @@
             this.CreateMap<ImportClientDto, Client>()
                 .ForMember(d => d.Addresses, opt => opt.Ignore());
 
-            //Invoice
-            this.CreateMap<ImportInvoiceDto, Invoice>()
-                .ForMember(d => d.CurrencyType,
-                opt => opt.MapFrom(s => (CurrencyType)s.CurrencyType));
-
             //Product
             this.CreateMap<ImportProductDto, Product>()
                 .ForMember(d => d.CategoryType, 
