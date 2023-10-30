f921d56303b9ed6ba8855eec7385faefdcb99b5e
nikolaiandrov <nikolaiandrov@abv.bg>
2023-10-30 13:49:41 +0200

    Creating the DB - Code First

--- InvoicesDatabasesAdvancedExam11April2023/Invoices/Data/InvoicesContext.cs
+++ InvoicesDatabasesAdvancedExam11April2023/Invoices/Data/InvoicesContext.cs
@@ -15,18 +15,32 @@ namespace Invoices.Data
         { 
         }
 
+        public virtual DbSet<Product> Products { get; set; } = null!;
+
+        public virtual DbSet<Invoice> Invoices { get; set; } = null!;
+
+        public virtual DbSet<Address> Addresses { get; set; } = null!;
+
+        public virtual DbSet<Client> Clients { get; set; } = null!;
+
+        public virtual DbSet<ProductClient> ProductsClients { get; set; } = null!;
+
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             if (!optionsBuilder.IsConfigured)
             {
                 optionsBuilder
+                    .UseLazyLoadingProxies()
                     .UseSqlServer(Configuration.ConnectionString);
             }
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-                  
+            modelBuilder.Entity<ProductClient>(entity =>
+            {
+                entity.HasKey(k => new { k.ProductId, k.ClientId });
+            });
         }
     }
 }
---
+++ InvoicesDatabasesAdvancedExam11April2023/Invoices/Data/Models/Address.cs
@@ -0,0 +1,37 @@
+﻿namespace Invoices.Data.Models
+{
+    using Invoices.Common;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+
+    public class Address
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(ValidationConstants.StreetNameMaxLength)]
+        public string StreetName { get; set; } = null!;
+
+        [Required]
+        public int StreetNumber { get; set; }
+
+        [Required]
+        public string PostCode { get; set; } = null!;
+
+        [Required]
+        [MaxLength(ValidationConstants.CityNameMaxLength)]
+        public string City { get; set; } = null!;
+
+        [Required]
+        [MaxLength(ValidationConstants.CountryNameMaxLength)]
+        public string Country { get; set; } = null!;
+
+        [Required]
+        public int ClientId { get; set; }
+
+        [Required]
+        [ForeignKey(nameof(ClientId))]
+        public virtual Client Client { get; set; } = null!;
+    }
+}
