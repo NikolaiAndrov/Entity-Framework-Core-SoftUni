21afe759fd5c2051f400980ddb34f70e7fc65394
nikolaiandrov <nikolaiandrov@abv.bg>
2023-11-05 14:31:44 +0200

    Getting done all the exports and finishing the whole exam preparation. Notice: many of the files"Expected Results" are not correct (for the most of the exams).

---
+++ Theatre-DatabasesAdvancedExam04December2021/Theatre/DataProcessor/ExportDto/ExportPlayDto.cs
@@ -0,0 +1,28 @@
+﻿namespace Theatre.DataProcessor.ExportDto
+{
+    using System.Xml.Serialization;
+
+    [XmlType("Play")]
+    public class ExportPlayDto
+    {
+        public ExportPlayDto()
+        {
+            this.Actors = new List<ExportActorDto>();
+        }
+
+        [XmlAttribute("Title")]
+        public string Title { get; set; } = null!;
+
+        [XmlAttribute("Duration")]
+        public string Duration { get; set; } = null!;
+
+        [XmlAttribute("Rating")]
+        public string Rating { get; set; } = null!;
+
+        [XmlAttribute("Genre")]
+        public string Genre { get; set; } = null!;
+
+        [XmlArray("Actors")]
+        public List<ExportActorDto> Actors { get; set; }
+    }
+}
--- Theatre-DatabasesAdvancedExam04December2021/Theatre/DataProcessor/Serializer.cs
+++ Theatre-DatabasesAdvancedExam04December2021/Theatre/DataProcessor/Serializer.cs
@@ -1,19 +1,72 @@
 ﻿namespace Theatre.DataProcessor
 {
-
+    using Newtonsoft.Json;
     using System;
+    using System.Diagnostics;
+    using System.Xml.Linq;
     using Theatre.Data;
+    using Theatre.Data.Models;
+    using Theatre.DataProcessor.ExportDto;
+    using Theatre.Utilities;
 
     public class Serializer
     {
         public static string ExportTheatres(TheatreContext context, int numbersOfHalls)
         {
-            throw new NotImplementedException();
+            var theatres = context.Theatres
+                .Where(t => t.NumberOfHalls >= numbersOfHalls && t.Tickets.Count() >= 20)
+                .Select(t => new
+                {
+                    Name = t.Name,
+                    Halls = t.NumberOfHalls,
+                    TotalIncome = t.Tickets
+                    .Where(t => t.RowNumber >= 1 && t.RowNumber <= 5)
+                    .Sum(t => t.Price),
+                    Tickets = t.Tickets
+                    .Where(t => t.RowNumber >= 1 && t.RowNumber <= 5)
+                    .Select(t => new
+                    {
+                        Price = t.Price,
+                        RowNumber = t.RowNumber
+                    })
+                    .OrderByDescending(t => t.Price)
+                    .ToArray()
+                })
+                .OrderByDescending(t => t.Halls)
+                .ThenBy(t => t.Name)
+                .ToArray();
+
+            return JsonConvert.SerializeObject(theatres, Formatting.Indented);
         }
 
         public static string ExportPlays(TheatreContext context, double raiting)
         {
-            throw new NotImplementedException();
+            var plays = context.Plays
+                .Where(p => p.Rating <= raiting)
+                .AsEnumerable()
+                .Select(p => new ExportPlayDto
+                {
+                    Title = p.Title,
+                    Duration = p.Duration.ToString("c"),
+                    Rating = p.Rating == 0 ? "Premier" : p.Rating.ToString(),
+                    Genre = p.Genre.ToString(),
+                    Actors = p.Casts
+                    .Where(c => c.IsMainCharacter)
+                    .Select(a => new ExportActorDto
+                    {
+                        FullName = a.FullName,
+                        MainCharacter = $"Plays main character in '{p.Title}'."
+                    })
+                    .OrderByDescending(a => a.FullName)
+                    .ToList()
+                })
+                .OrderBy(p => p.Title)
+                .ThenByDescending(p => p.Genre)
+                .ToList();
+
+            XmlParser xmlParser = new XmlParser();
+
+            return xmlParser.Serialize(plays, "Plays");
         }
     }
 }
